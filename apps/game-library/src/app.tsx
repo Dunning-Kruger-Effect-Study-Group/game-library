21a4a52a7b1d6b29aa7905c6bb2a2a2395a74bbd
Victor Ross <vicjun22@gmail.com>
2023-11-09 22:37:09 -0300

    start commit about redux toolkit

--- apps/game-library/src/app.tsx
+++ apps/game-library/src/app.tsx
@@ -1,18 +1,22 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import { Base } from './features/ui/base';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 export function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Base />}>
-          <Route index element={<Home />} />
-          {/* <Route path="Hobby" element={<Hobby />} />
-          <Route path="*" element={<NoPage />} /> */}
-        </Route>
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Base />}>
+            <Route index element={<Home />} />
+            {/* <Route path="Hobby" element={<Hobby />} />
+            <Route path="*" element={<NoPage />} /> */}
+          </Route>
+        </Routes>
+      </Provider>
     </BrowserRouter>
   );
 }
