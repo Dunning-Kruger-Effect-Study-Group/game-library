32be3b2e09d4e264f6892ba84dc8fbb93c72344e
Victor Ross <vicjun22@gmail.com>
2023-11-15 23:01:05 -0300

    Manutenção do app

--- apps/game-library/src/app.tsx
+++ apps/game-library/src/app.tsx
@@ -1,41 +1,25 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-<<<<<<< HEAD
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
-import { Base } from './features/ui/base';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-=======
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 import { Base } from './features/ui/base';
 import { Games } from './pages/games';
 import Companies from './pages/companies';
 import Platforms from './pages/platforms';
->>>>>>> 99ec0eb77ecc4bd0e2e6446d9a1fcbc16f79e59c
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 export function App() {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
       <Provider store={store}>
         <Routes>
           <Route path="/" element={<Base />}>
-            <Route index element={<Home />} />
-            {/* <Route path="Hobby" element={<Hobby />} />
-            <Route path="*" element={<NoPage />} /> */}
+            <Route index path='/games' element={<Games />} />
+            <Route path="/platforms" element={<Platforms />} />
+            <Route path="/companies" element={<Companies />} />
+            {/* <Route path="*" element={<NoPage />} /> */}
           </Route>
         </Routes>
       </Provider>
-=======
-      <Routes>
-        <Route path="/" element={<Base />}>
-          <Route index path='/games' element={<Games />} />
-          <Route path="/platforms" element={<Platforms />} />
-          <Route path="/companies" element={<Companies />} />
-          {/* <Route path="*" element={<NoPage />} /> */}
-        </Route>
-      </Routes>
->>>>>>> 99ec0eb77ecc4bd0e2e6446d9a1fcbc16f79e59c
     </BrowserRouter>
   );
 }
