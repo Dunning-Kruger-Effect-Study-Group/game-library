63fdbb637ffcbcc0c128c002be780dbe74acd6fc
Mateus Martini Hoff <114887417+marthff@users.noreply.github.com>
2023-12-01 20:18:58 -0300

    games control-bar

--- apps/game-library/src/pages/games.tsx
+++ apps/game-library/src/pages/games.tsx
@@ -1,7 +1,14 @@
 import { IGameData } from '@game-library/types';
 import { GameCard } from '../features/collection/gameCard';
+import { CompanyCard } from '../features/collection/companyCard';
+import { show } from '../redux/slices/ui/overlaySlice';
+import { useAppDispatch } from '../redux/hooks';
+
+
 
 export function Games() {
+  const dispatch = useAppDispatch();
+
   const arr: Array<IGameData> = [
     {
       coverArt:
@@ -14,10 +21,22 @@ export function Games() {
   ];
 
   return (
+    <div className="flex flex-col">
+      <div className="bg-Dark_Blue  mt-1">
+        <button
+          onClick={(e) => {
+            dispatch(show());
+          }}
+          className="p-4 hover:bg-Baby_Blue hover:text-Dark_Blue"
+        >
+          New
+        </button>
+      </div>
     <div className="w-full h-full grid sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
       {arr.map((game) => {
         return <GameCard gameData={game}></GameCard>;
       })}
+    </div>
     </div>
   );
 }
